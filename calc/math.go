adfea00e250c7cdca0e10fe298251f7109967c9e
rihou <rihou@cisco.com>
2020-05-10 19:21:45 -0400

    test

--- calc/math.go
+++ calc/math.go
@@ -1,12 +1,14 @@
 package calc
 
 import "errors"
+import "github.com/fatih/color"
 
 func Add(numbers ...int) (error, int) {
 	sum := 0
 
 	if len(numbers) < 2 {
-		return errors.New("provide more than 2 numbers"), sum
+		errorMessage := color.RedString("provide more than 2 numbers")
+		return errors.New(errorMessage), sum
 	} else {
 		for _, num := range numbers {
 			sum = sum + num
